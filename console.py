e62d257408bb826a2c395327a0e98676d5a17d0d
“Darkidd77” <m.almahdi77@yahoo.com>
2024-03-10 23:15:04 +0000

    task-console-001-fix

--- console.py
+++ console.py
@@ -8,27 +8,9 @@ from models.place import Place
 from models.amenity import Amenity
 from models.review import Review
 import re
-from shlex import split
 from models import storage
 from models.base_model import BaseModel
 
-
-def parse(arg):
-    curly_braces = re.search(r"\{(.*?)\}", arg)
-    brackets = re.search(r"\[(.*?)\]", arg)
-    if curly_braces is None:
-        if brackets is None:
-            return [i.strip(",") for i in split(arg)]
-        else:
-            lexer = split(arg[:brackets.span()[0]])
-            retl = [i.strip(",") for i in lexer]
-            retl.append(brackets.group())
-            return retl
-    else:
-        lexer = split(arg[:curly_braces.span()[0]])
-        retl = [i.strip(",") for i in lexer]
-        retl.append(curly_braces.group())
-        return retl
 
 
 class HBNBCommand(cmd.Cmd):
@@ -52,27 +34,6 @@ class HBNBCommand(cmd.Cmd):
     def emptyline(self):
         """Do nothing when receiving an empty line."""
         pass
-
-    def default(self, arg):
-        """Default behavior cmd module when input invalid"""
-        argdict = {
-            "all": self.do_all,
-            "show": self.do_show,
-            "destroy": self.do_destroy,
-            "count": self.do_count,
-            "update": self.do_update
-        }
-        match = re.search(r"\.", arg)
-        if match is not None:
-            argl = [arg[:match.span()[0]], arg[match.span()[1]:]]
-            match = re.search(r"\((.*?)\)", argl[1])
-            if match is not None:
-                command = [argl[1][:match.span()[0]], match.group()[1:-1]]
-                if command[0] in argdict.keys():
-                    call = "{} {}".format(argl[0], command[1])
-                    return argdict[command[0]](call)
-        print("*** Unknown syntax: {}".format(arg))
-        return False
 
     def do_quit(self, arg):
         """Quit command to exit."""
